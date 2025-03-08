d443c65fe32208fdeb8f25d5c2039db04b34d712
VF <fanaskov.vladimir@gmail.com>
2025-03-08 00:24:15 +0000

    non-associative training loop

--- learning/training_loops.py
+++ learning/training_loops.py
@@ -10,7 +10,7 @@ import sys
 from jax import random
 from jax.lax import dot_general
 from architectures import Hopfield, Kuramoto, Hopfield_Kuramoto
-from learning import classification
+from learning import classification, non_associative_edditing
 from jax.tree_util import tree_map, tree_flatten
 
 def load_MNIST(path_to_MNIST):
@@ -40,6 +40,20 @@ def load_MNIST(path_to_MNIST):
     
     targets = jnp.concatenate([targets, targets_], axis=0)
     return features, targets
+
+def conflate_digits(targets, conflation_list):
+    d0 = conflation_list[0]
+    for d in conflation_list[1:]:
+        targets = targets.at[targets == d].set(d0)
+    return targets
+
+def swap_digits(targets, digits_a, digits_b):
+    for a, b in zip(digits_a, digits_b):
+        mask_a = targets == a
+        mask_b = targets == b
+        targets = targets.at[mask_a].set(b)
+        targets = targets.at[mask_b].set(a)
+    return targets
 
 def get_standard_args():
     args = {
@@ -80,6 +94,16 @@ def measure_accuracy(model, features, targets, ind, args_, args, solver_data, pr
             predicted.append(prediction)
         predicted = jnp.concatenate(predicted)
         acc = targets[ind.reshape(-1,)] == predicted
+    return acc
+
+def non_associative_measure_accuracy(model, features, targets, ind, args_, args, solver_data, preprocessing, postprocessing, key):
+    predicted = []
+    keys = random.split(key, ind.shape[0])
+    for i, key in zip(ind, keys):
+        prediction = non_associative_edditing.predict_class(model, features[i], args_, solver_data, preprocessing, postprocessing, key)
+        predicted.append(prediction)
+    predicted = jnp.concatenate(predicted)
+    acc = targets[ind.reshape(-1,)] == predicted
     return acc
 
 def Hopfield_dense(args):
@@ -181,6 +205,119 @@ def Hopfield_dense(args):
         
     train_acc = measure_accuracy(model, features, targets, inds_train_check, args_, args, solver_data, preprocessing, postprocessing, key3)
     test_acc = measure_accuracy(model, features, targets, inds_test_check, args_, args, solver_data, preprocessing, postprocessing, key4)
+
+    with open(write_logs_to, "a+") as f:
+        f.write(f"total train accuracy {jnp.mean(train_acc)}, total test accuracy {jnp.mean(test_acc)}\n")
+    training_summary["train_accuracy"] = jnp.mean(train_acc).item()
+    training_summary["test_accuracy"] = jnp.mean(test_acc).item()
+    
+    return model, opt_state, losses, training_summary
+
+def non_associative_Hopfield_Kuramoto(args):
+    training_summary = {}
+    write_logs_to = args["write_logs_to"]
+    path_to_MNIST = args["path_to_MNIST"]
+    features_H, targets = load_MNIST(path_to_MNIST)
+    with open(write_logs_to, "a+") as f:
+        f.write("loading MNIST\n")
+
+    if args["editing_type"] == "swap":
+        targets = swap_digits(targets, args["swap_list"][0], args["swap_list"][1])
+    elif args["editing_type"] == "conflation":
+        targets = conflate_digits(targets, args["conflation_list"])
+        
+    keys = random.split(random.PRNGKey(args["key"]), 6)
+    key = keys[-1]
+    key_ = keys[-2]
+
+    N_features = features_H.shape[1] + args["N_augment"] + args["N_classes"]
+
+    if args["interaction"] == "relu":
+        interaction = Kuramoto.relu_interaction
+    elif args["interaction"] == "tanh":
+        interaction = Kuramoto.tanh_interaction
+    elif args["interaction"] == "sigmoid":
+        interaction = Kuramoto.sigmoid_interaction
+
+    if args["activation"] == "relu":
+        LNet = Hopfield.Lagrange_relu
+    elif args["activation"] == "tanh":
+        LNet = Hopfield.Lagrange_tanh
+    elif args["activation"] == "sigmoid":
+        LNet = Hopfield.Lagrange_sigmoid
+
+    model_H = Hopfield.Hopfield_dense(N_features, LNet, key)
+    model_H = eqx.tree_deserialise_leaves(args["model_path"], model_H)
+
+    ind_K = Kuramoto.get_small_world_connectivity(keys[0], N_features, k=args["k"])
+    ind_HK = Kuramoto.get_small_world_connectivity(keys[1], N_features, k=args["k"])
+    eps_K = 1 / jnp.sqrt(args["k"])
+    eps_H = 1 / jnp.sqrt(N_features)
+    eps_HK = args["eps_HK"]
+    N_weights = ind_K.shape[0]
+    N_weights_i = ind_HK.shape[0]
+
+    if args["coupling_type"] == "additive":
+        model = Hopfield_Kuramoto.Hopfield_Kuramoto_additive(N_weights, interaction, N_features, args["D"], keys[2], eps_K, eps_HK)
+    elif args["coupling_type"] == "multiplicative":
+        model = Hopfield_Kuramoto.Hopfield_Kuramoto_multiplicative(N_weights, interaction, N_features, args["D"], keys[2], eps_K, eps_HK)
+    model_size = sum(tree_map(lambda x: 2*x.size if x.dtype == jnp.complex64 else x.size, tree_flatten(model)[0]))
+    training_summary["model_size"] = model_size
+    with open(write_logs_to, "a+") as f:
+        f.write(f"model size {model_size}\n")
+
+    sc = optax.exponential_decay(args["learning_rate"], args["N_drop"], args["gamma"])
+    if args["optim"] == "lion":
+        optim = optax.lion(learning_rate=sc)
+    elif args["optim"] == "adam":
+        optim = optax.adam(learning_rate=sc)
+    opt_state = optim.init(eqx.filter(model, eqx.is_array))
+   
+    preprocessing = lambda feature, args_: classification.Hopfield_preprocessing(feature, args["N_augment"], args["N_classes"])
+    postprocessing = lambda prediction: classification.Hopfield_Kuramoto_postprocessing(prediction, args["N_classes"])
+    solver_data = non_associative_edditing.get_default_solver_data()
+    args_ = [ind_K, ind_HK, args["kappa_K"], args["kappa_H"], model_H, None]
+
+    inds = random.choice(keys[1], args["N_train"], (args["N_updates"], args["N_batch"]))
+    losses = []
+    times = []
+    for step, n in enumerate(inds):
+        start = time.time()
+        loss, model, opt_state = non_associative_edditing.make_step(model, features_H[n], targets[n], args_, solver_data, preprocessing, postprocessing, None, opt_state, optim)
+        end = time.time()
+        times.append(end - start)
+        losses.append(loss)
+        if (step % args["print_every"]) == 0 or step == args["N_updates"] - 1:
+            with open(write_logs_to, "a+") as f:
+                f.write(f"Step: {step}, Loss: {loss}, Computation time: {end - start}\n")
+    
+            N_samples = 5
+            N_batch_predict = 100
+            key, key1, key2, key3, key4 = random.split(key, 5)
+            
+            inds_train_check = random.choice(key1, args["N_train"], (N_samples, N_batch_predict))
+            inds_test_check = random.choice(key2, features_H.shape[0] - args["N_train"], (N_samples, N_batch_predict)) + args["N_train"]
+
+            train_acc = non_associative_measure_accuracy(model, features_H, targets, inds_train_check, args_, args, solver_data, preprocessing, postprocessing, key3)
+            test_acc = non_associative_measure_accuracy(model, features_H, targets, inds_test_check, args_, args, solver_data, preprocessing, postprocessing, key4)
+            
+            with open(write_logs_to, "a+") as f:
+                f.write(f"train accuracy {jnp.mean(train_acc)}, test accuracy {jnp.mean(test_acc)}\n")
+    losses = jnp.array(losses)
+    
+    with open(write_logs_to, "a+") as f:
+        f.write(f"total training time {sum(times)}\n")
+    training_summary["training_time"] = sum(times)
+
+    N_samples = 10
+    N_batch_predict = 100
+    key, key1, key2, key3, key4 = random.split(key, 5)
+
+    inds_train_check = jnp.arange(args["N_train"]).reshape(-1, 100)
+    inds_test_check = args["N_train"] + jnp.arange(args["N_test"]).reshape(-1, 100)
+        
+    train_acc = non_associative_measure_accuracy(model, features_H, targets, inds_train_check, args_, args, solver_data, preprocessing, postprocessing, key3)
+    test_acc = non_associative_measure_accuracy(model, features_H, targets, inds_test_check, args_, args, solver_data, preprocessing, postprocessing, key4)
 
     with open(write_logs_to, "a+") as f:
         f.write(f"total train accuracy {jnp.mean(train_acc)}, total test accuracy {jnp.mean(test_acc)}\n")
